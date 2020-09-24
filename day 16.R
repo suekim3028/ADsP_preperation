88adf8a25d223c166441f3ebae4d1754fa9856ef
suebin <sue_bin1029@yonsei.ac.kr>
2020-09-25 03:27:47 +0900

    day 16

---
+++ day 16.R
@@ -0,0 +1,8 @@
+klaR #클래스 분류 에러율 계산 + 그래프로 결과 보여줌
+greedy.wilks #wilks lambda 이용 변수 중요도 정리 
+
+complete.cases() #결측값 없으면 TRUE
+is.na() #결측값 있으면 true
+centralImputation() #중위수 or 최빈값 대치
+knnImputation()
+amelia() #랜덤포레스트 rfImpute()함수 활용 결측값 대치
